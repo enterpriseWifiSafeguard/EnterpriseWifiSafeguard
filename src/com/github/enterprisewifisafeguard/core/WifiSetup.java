bb19d06440b37b5dd595cc910426be9178137529
xamor <maximilian@ortwein-online.de>
2015-08-18 14:29:56 +0200

    finished network saving method

--- src/com/github/enterprisewifisafeguard/core/WifiSetup.java
+++ src/com/github/enterprisewifisafeguard/core/WifiSetup.java
@@ -1,26 +1,59 @@
 package com.github.enterprisewifisafeguard.core;
 
+import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.List;
 
 import android.content.Context;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.util.Log;
 
 public class WifiSetup {
 private boolean error = false;	
 private WifiManager wifiObj = null;
+private Context context;
 
 public WifiSetup(Context context) {
 	wifiObj = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+	this.context = context;
 }
 
-//TODO add arguments to the function!!!! this function handles user input and will be called from the ui
-public boolean createConnection() {
-	//TODO enable wifi
-	//TODO remove
-	//TODO create
-	//TODO add
-	//TODO save
+public boolean createConnection(String ssid, String username, String password, String anonymous, String cn, int eap_meth, int phase2) {
+	WifiConfig entConfig = new WifiConfig();
+	WifiConfiguration wifi = this.getWifiConfiguration(ssid);
+	//enable Wifi
+	if(!wifiObj.isWifiEnabled()){
+		wifiObj.setWifiEnabled(true);
+	}
+	//remove if wifi exists
+	if(wifi !=null) {
+		error = !this.removeWifi(wifi.networkId);
+	}
+	//create new Wifi Configuration
+	entConfig.setAnonymous_ident(anonymous);
+	entConfig.setUsername(username);
+	entConfig.setPassword(password);
+	entConfig.setSubject_match(cn);
+	entConfig.setEap_method(eap_meth);
+	entConfig.setPhase2_method(phase2);
+	try {
+	entConfig.setCa_certificate(this.getRootCA(this.context));
+	wifi = entConfig.createNewWifiProfile();
+	}
+	catch (Exception e) {
+		error = true;
+		return error;
+	}
+	//add wifi network
+	int id = wifiObj.addNetwork(wifi);
+	if (id == -1) {
+		error = true;
+	}
+	//save network configurations
+	error = wifiObj.saveConfiguration();
 	return error;
 }
 
@@ -50,4 +83,14 @@ public int addWifi (WifiConfiguration config) {
 }
 
 //TODO Cerfificate stuff
+
+private X509Certificate getRootCA(Context context) throws CertificateException {
+    CertificateFactory ca = null;
+    X509Certificate cert = null;
+    
+        ca = CertificateFactory.getInstance("X.509");
+      //TODO  InputStream certStream = context.getResources().openRawResource(R.raw.);
+      //TODO  cert = (X509Certificate) ca.generateCertificate(certStream);
+      return cert;       
+}
 }
